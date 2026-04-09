d38340927d8be26c537eabf044f7eb36854fe6d8
abduhamid.raximov <abduhamid.raximov@uzinfocom.uz>
2026-04-09 22:29:47 +0500

    add abckedn urls to cors allowed origins

--- backend/config/cors.py
+++ backend/config/cors.py
@@ -29,7 +29,7 @@ class DevCorsMiddleware:
             item.strip()
             for item in os.environ.get(
                 'CORS_ALLOWED_ORIGINS',
-                'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000',
+                'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000,https://mchs-3ewf.onrender.com/'
             ).split(',')
             if item.strip()
         ]
